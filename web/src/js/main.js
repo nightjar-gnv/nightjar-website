0dca28f0f139c9e9c2998dc7d22ccbb1b6bd0558
Ryan Hall <ryanhallcs@gmail.com>
2025-07-18 18:08:01 -0400

    feat: initial layout and content

---
+++ web/src/js/main.js
@@ -0,0 +1,55 @@
+// Import our custom CSS
+import '../scss/styles.scss'
+
+// Import all of Bootstrap’s JS
+import * as bootstrap from 'bootstrap'
+
+window.addEventListener('asdf', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            console.log('collapsible not found')
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+            console.log('collapsible shrink removed')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+            console.log('collapsible shrink added')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
