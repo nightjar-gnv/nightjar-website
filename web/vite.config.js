0deef387b0c22e905d466c41ed9eebcc6bcfe511
arthur-maps <52044975+arthur-maps@users.noreply.github.com>
2026-06-03 12:54:16 -0400

    Update vite.config.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        // Your default entry point
+        main: resolve(__dirname, 'index.html'), 
+        
+        // Option A: An additional HTML page in the root directory
+        residency: resolve(__dirname, 'residency.html')
+      }
+    }
+  },
   base: "./",
   plugins: [],
   // Optional: Silence Sass deprecation warnings. See note below.
